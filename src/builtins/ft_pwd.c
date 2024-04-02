230764c68e0c158cd6923ef6ff8e65d9e184e9a6
Daiana <dairotchen@gmail.com>
2024-04-02 11:46:31 +0200

    short pwd

--- src/builtins/ft_pwd.c
+++ src/builtins/ft_pwd.c
@@ -1,31 +1,12 @@
 #include "minishell.h"
 
-void ft_pwd(t_data data)
+void ft_pwd(t_data *data)
 {
-    char cwd[MAXPATHLEN];
-   // int read_e = data->commands[0].pipe_out;
-    int write_e = data.commands[1].pipe_in;
-    // printf("PIPE 1 read_e: %d\n", read_e);
-    //printf("PIPE 2 write_e: %d\n", write_e);
+        (void) data;
+        char cwd[MAXPATHLEN];
 
-    if (getcwd(cwd, sizeof(cwd)) != NULL)
-    {
-        if (write_e != -1)
-        {
-            if (write(write_e, cwd, strlen(cwd)) == -1)
-            {
-                perror(RED "write error in pwd" RESET);
-                exit(1);
-            }
-            printf(BLU "\nWritting in" RESET " write_end: %d\n", write_e);
-        } 
+        if (getcwd(cwd, sizeof(cwd)) != NULL)
+                printf( "%s\n", cwd);
         else
-            printf( "%s\n", cwd);
-      //  exit(0);
-    }
-    else
-    {
         perror("getcwd error");
-        exit(0);
-    }
 }
