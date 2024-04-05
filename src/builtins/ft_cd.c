39160721fca2b68ec49cbaad35a70ed22ebf674d
Daiana <dairotchen@gmail.com>
2024-04-05 23:51:51 +0200

    passing env success

--- src/builtins/ft_cd.c
+++ src/builtins/ft_cd.c
@@ -1,15 +1,13 @@
 #include "minishell.h"
 
-void ch_env_pwd(t_data *data, char *new_path)
+void ch_env_pwd(t_data *data, char *new_pwd, char *old_pwd)
 {
     char **envp;
     int i;
 
     envp = data->envs->var;
     i = 0;
-    char *new_env = ft_strjoin("PWD=", new_path);
-    char *old_env;
-    old_env = getenv("PWD");
+    char *new_env = ft_strjoin("PWD=", new_pwd);
     if (envp == NULL) {
         printf(RED "envp is null in ch_env_pwd()\n" RESET);
         exit(1);
@@ -17,70 +15,53 @@ void ch_env_pwd(t_data *data, char *new_path)
     while (envp[i] != NULL)
     {
         if (ft_strncmp(envp[i], "PWD=", 4) == 0)
-            envp[i] = new_env;
+            envp[i] = ft_strdup(new_env);
         else if (ft_strncmp(envp[i], "OLDPWD=", 7) == 0)
-            envp[i] = ft_strjoin("OLDPWD=", old_env);
+            envp[i] = ft_strjoin("OLDPWD=", old_pwd);
         i++;
     }
+    //free(data->envs->var);
     data->envs->var = envp;
 }
 
 void ft_cd(t_data *data, t_cmd *cmd)
 {
+    char *old_pwd;
+    char *new_pwd;
+
+    old_pwd = getcwd(NULL, 0);
+    //old_pwd = getcwd(cwd, sizeof(cwd));
+    if (old_pwd == NULL)
+    {
+        perror("Failed to get current directory");
+        return;
+    }
     if (cmd->args[1] == NULL)
         return;
-    //printf(GRN "Directory before cd: " RESET);
+    printf(BLU "Directory before cd: " RESET);
+    printf("%s\n", old_pwd);
+    //printf("%s\n", new_pwd);
     //ft_pwd(data);
-    //printf(GRN "Enviroment PWD before: " RESET "%s\n", getenv("PWD"));
-    if (chdir(cmd->args[1]) != 0)
-        printf(RED "%s: No such file or director" RESET, cmd->args[1]);
-    ch_env_pwd(data, cmd->args[1]);
-    //printf(GRN "Directory after cd: " RESET);
-    //ft_pwd(data);
-    //printf(GRN "Enviroment PWD after: " RESET "%s\n", getenv("PWD"));
+    // printf(BLU "Enviroment PWD before: " RESET "%s\n", getenv("PWD"));
+    //     ft_env(data);
+    if (chdir(cmd->args[1]) != 0) {
+        perror("chdir failed");
+        free(old_pwd);
+        return;
+    }
+    new_pwd = getcwd(NULL, 0); // Obtiene el nuevo directorio
+    if (new_pwd == NULL) {
+        perror("Failed to get current directory");
+        free(old_pwd);
+        return;
+    }
+    ch_env_pwd(data, new_pwd, old_pwd);
+    printf(GRN "Directory after cd: " RESET);
+    ft_pwd(data);
+
+    printf(GRN "Enviroment PWD after: " RESET "%s\n", getenv("PWD"));
     ft_env(data);
+    free(new_pwd);
+    free(old_pwd);
+
 }
-
-// ///////////// BACKUP COPY
-// // #include "minishell.h"
-
-// // void ch_env_pwd(t_data *data, char *new_path)
-// // {
-// //     char **envp;
-// //     int i;
-
-// //     envp = data->envp;
-// //     i = 0;
-// //     char *new_env = ft_strjoin("PWD=", new_path);
-// //     char *old_env;
-// //     old_env = getenv("PWD");
-// //     if (envp == NULL) {
-// //         printf(RED "envp is null in ch_env_pwd()\n" RESET);
-// //         exit(1);
-// //     }
-// //     while (envp[i] != NULL)
-// //     {
-// //         if (ft_strncmp(envp[i], "PWD=", 4) == 0)
-// //             envp[i] = new_env;
-// //         else if (ft_strncmp(envp[i], "OLDPWD=", 7) == 0)
-// //             envp[i] = ft_strjoin("OLDPWD=", old_env);
-// //         i++;
-// //     }
-// //     data->envp = envp;
-// // }
-
-// // void ft_cd(t_data *data, t_cmd *cmd)
-// // {
-// //     if (cmd->args[1] == NULL)
-// //         return;
-// //     //printf(GRN "Directory before cd: " RESET);
-// //     //ft_pwd(data);
-// //     //printf(GRN "Enviroment PWD before: " RESET "%s\n", getenv("PWD"));
-// //     if (chdir(cmd->args[1]) != 0)
-// //         printf(RED "%s: No such file or director" RESET, cmd->args[1]);
-// //     ch_env_pwd(data, cmd->args[1]);
-// //     //printf(GRN "Directory after cd: " RESET);
-// //     //ft_pwd(data);
-// //     //printf(GRN "Enviroment PWD after: " RESET "%s\n", getenv("PWD"));
-// //     ft_env(data);
-// // }
