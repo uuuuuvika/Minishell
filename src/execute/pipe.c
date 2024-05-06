fe5f153004492238f36bcbc57eaaf81101a72701
Vika Shcherbak <vikashcherbaka@gmail.com>
2024-05-06 21:35:28 +0200

    init commit for a pipe fix

--- src/execute/pipe.c
+++ src/execute/pipe.c
@@ -22,6 +22,7 @@ int pipe_cmds(t_data *data)
         {
             if (current->pipe_in != -1)
             {
+                // close(current->pipe_out);
                 if (dup2(current->pipe_in, STDIN) == -1)
                     handle_error("dup2 error pipe_in");
                 close(current->pipe_in);
@@ -46,31 +47,70 @@ int pipe_cmds(t_data *data)
                     handle_error("dup2 error pipe_out");
                 close(current->pipe_out);
             }
+
             exit(exec_cmd(data, current));
         }
         else
         {
-            if (current->pipe_out != -1)
-                close(current->pipe_out);
-            if (current->pipe_in != -1)
-                close(current->pipe_in);
-            if (current->redirect_in != -1)
-                close(current->redirect_in);
-            if (current->redirect_out != -1)
-                close(current->redirect_out);
+            // if (waitpid(pid, &exit_code, 0) == -1)
+            // {
+            //     perror("waitpid failed");
+            //     data->exit_code = 1;
+            //     return EXIT_FAILURE;
+            // }
+            // if (WIFEXITED(exit_code))
+            // {
+            //     data->exit_code = WEXITSTATUS(exit_code);
+            // }
+            do
+            {
+                pid_t w = waitpid(pid, &exit_code, WUNTRACED | WCONTINUED);
+                if (w == -1)
+                {
+                    perror("waitpid");
+                    exit(EXIT_FAILURE);
+                }
+
+                if (WIFEXITED(exit_code))
+                {
+                    printf("exited, status=%d\n", WEXITSTATUS(exit_code));
+                }
+                else if (WIFSIGNALED(exit_code))
+                {
+                    printf("killed by signal %d\n", WTERMSIG(exit_code));
+                }
+                else if (WIFSTOPPED(exit_code))
+                {
+                    printf("stopped by signal %d\n", WSTOPSIG(exit_code));
+                }
+                else if (WIFCONTINUED(exit_code))
+                {
+                    printf("continued\n");
+                }
+            } while (!WIFEXITED(exit_code) && !WIFSIGNALED(exit_code));
+            //exit(EXIT_SUCCESS);
+            // if (current->pipe_out != -1)
+            //     close(current->pipe_out);
+            // if (current->pipe_in != -1)
+            //     close(current->pipe_in);
+            // if (current->redirect_in != -1)
+            //     close(current->redirect_in);
+            // if (current->redirect_out != -1)
+            //     close(current->redirect_out);
         }
         current = current->next;
     }
 
-    if (waitpid(pid, &exit_code, 0) == -1)
-    {
-        perror("waitpid failed");
-        return EXIT_FAILURE;
-    }
+    // if (waitpid(pid, &exit_code, 0) == -1)
+    // {
+    //     perror("waitpid failed");
+    //     data->exit_code = 1;
+    //     return EXIT_FAILURE;
+    // }
 
-    if (WIFEXITED(exit_code))
-    {
-        data->exit_code = WEXITSTATUS(exit_code);
-    }
+    // if (WIFEXITED(exit_code))
+    // {
+    //     data->exit_code = WEXITSTATUS(exit_code);
+    // }
     return EXIT_SUCCESS;
 }
