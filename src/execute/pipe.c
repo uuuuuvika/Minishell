e8d4c22588d772f81a3da68959a04c77c19ec638
vika <vikashcherbaka@gmail.com>
2024-05-11 00:33:19 +0200

    add pipe utils

--- src/execute/pipe.c
+++ src/execute/pipe.c
@@ -1,38 +1,13 @@
 #include "minishell.h"
 
-void handle_error(const char *message)
-{
-    perror(message);
-    exit(EXIT_FAILURE);
-}
-
-void close_pipes(t_data *data)
-{
-    t_cmd *current;
-
-    current = data->commands;
-    while (current != NULL)
-    {
-        if (current->pipe_in != -1)
-            close(current->pipe_in);
-        if (current->pipe_out != -1)
-            close(current->pipe_out);
-        if (current->redirect_in != -1)
-            close(current->redirect_in);
-        if (current->redirect_out != -1)
-            close(current->redirect_out);
-        current = current->next;
-    }
-}
-
 int pipe_cmds(t_data *data)
 {
-    t_cmd   *current;
-    pid_t   pid[data->num_of_children];
-    int     exit_code;
+    t_cmd *current;
+    pid_t pid[data->num_of_children];
+    int i;
 
+    i = 0;
     current = data->commands;
-    int i = 0;
     while (current != NULL)
     {
         pid[i] = fork();
@@ -40,58 +15,15 @@ int pipe_cmds(t_data *data)
             return EXIT_FAILURE;
         else if (pid[i] == 0)
         {
-            if (current->pipe_in != -1)
-            {
-                if (dup2(current->pipe_in, STDIN) == -1)
-                    handle_error("dup2 error pipe_in");
-            }
-            else if (current->redirect_in != -1)
-            {
-                if (dup2(current->redirect_in, STDIN) == -1)
-                    handle_error("dup2 error redirect_in");
-            }
-
-            if (current->redirect_out != -1)
-            {
-                if (dup2(current->redirect_out, STDOUT) == -1)
-                    handle_error("dup2 error redirect_out");
-            }
-            else if (current->pipe_out != -1)
-            {
-                if (dup2(current->pipe_out, STDOUT) == -1)
-                    handle_error("dup2 error pipe_out");
-            }
-            close_pipes(data);
-            //printf("executing command: %s\n", current->args[0]);
-            exit(exec_cmd(data, current));
+            handle_dup2(current);
+            ultimate_fd_close(data);
+            return (exec_cmd(data, current));
         }
         else
-        {
-            if (current->pipe_out != -1)
-                close(current->pipe_out);
-            if (current->pipe_in != -1)
-                close(current->pipe_in);
-            if (current->redirect_in != -1)
-                close(current->redirect_in);
-            if (current->redirect_out != -1)
-                close(current->redirect_out);
-        }
+            fd_close(current);
         current = current->next;
         i++;
     }
-
-    i = 0;
-    while (i < data->num_of_children)
-    {
-        //printf("pid[%d]: %d\n", i, pid[i]);
-        if (waitpid(pid[i], &exit_code, 0) == -1)
-        {
-            perror("waitpid failed");
-            return EXIT_FAILURE;
-        }
-        if (WIFEXITED(exit_code))
-            data->exit_code = WEXITSTATUS(exit_code);
-        i++;
-    }
+    ultimate_wait(data, pid);
     return EXIT_SUCCESS;
 }
---
+++ src/execute/pipe_utils.c
@@ -0,0 +1,65 @@
+#include "minishell.h"
+
+void fd_close(t_cmd *current)
+{
+    if (current->pipe_in != -1)
+        close(current->pipe_in);
+    if (current->pipe_out != -1)
+        close(current->pipe_out);
+    if (current->redirect_in != -1)
+        close(current->redirect_in);
+    if (current->redirect_out != -1)
+        close(current->redirect_out);
+}
+
+void ultimate_fd_close(t_data *data)
+{
+    t_cmd *current;
+
+    current = data->commands;
+    while (current != NULL)
+    {
+        fd_close(current);
+        current = current->next;
+    }
+}
+
+void ultimate_wait(t_data *data, pid_t *pid)
+{
+    int i;
+    int exit_code;
+
+    i = 0;
+    while (i < data->num_of_children)
+    {
+        if (waitpid(pid[i], &exit_code, 0) == -1)
+            handle_error("waitpid failed");
+        if (WIFEXITED(exit_code))
+            data->exit_code = WEXITSTATUS(exit_code);
+        i++;
+    }
+}
+
+void handle_dup2(t_cmd *current)
+{
+    if (current->pipe_in != -1)
+    {
+        if (dup2(current->pipe_in, STDIN) == -1)
+            handle_error("dup2 error pipe_in");
+    }
+    else if (current->redirect_in != -1)
+    {
+        if (dup2(current->redirect_in, STDIN) == -1)
+            handle_error("dup2 error redirect_in");
+    }
+    if (current->redirect_out != -1)
+    {
+        if (dup2(current->redirect_out, STDOUT) == -1)
+            handle_error("dup2 error redirect_out");
+    }
+    else if (current->pipe_out != -1)
+    {
+        if (dup2(current->pipe_out, STDOUT) == -1)
+            handle_error("dup2 error pipe_out");
+    }
+}
