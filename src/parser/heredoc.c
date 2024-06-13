b2b2d5e1ff2a2c0cfce733ab74529b5df99b07b5
Daiana Rotchen <darotche@c4b11c1.42berlin.de>
2024-06-13 11:45:51 +0200

    fix in heredoc exiting with ctrl-C

--- src/parser/heredoc.c
+++ src/parser/heredoc.c
@@ -28,13 +28,33 @@ void read_heredoc_simple(char *delimiter, t_data *data)
 	close(fd);
 }
 
+void sig_handler_heredoc(int sig)
+{
+	if (sig == SIGINT)
+  	{
+	//	printf(MAG"handle_ctrl\n"RESET);////////////
+		ioctl(0, TIOCSTI, "\n");
+		rl_replace_line("", 1);// We need this line to clear the  input line when typing heredoc input but not hitting enter
+		rl_on_new_line(); /// This line is for when we press ctrl+c before inputting anything
+		//rl_redisplay();
+		g_signal = 2;
+		//printf(CYN "g_singal %d" RESET,g_signal);//////////////
+	}
+	else if (sig == SIGPIPE)
+	{
+		printf(MAG"SIGPIPE\n"RESET);
+		g_signal = 1;
+	}
+}
+
+
 void read_heredoc(char *delimiter, t_cmd *current, t_data *data)
 {
 	char *line;
 	int fd;
 	fd = open("here_doc", O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	current->here_doc = fd; // probably can be replaced by data->cmn_here_doc
-
+	g_signal = 0; // Important to reset signal
 	while (1)
 	{
 		line = readline("> ");
