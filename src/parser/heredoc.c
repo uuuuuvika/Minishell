bb831510beb01adc7a0ff666f805036b7a636394
Daiana Rotchen <darotche@c4b11c1.42berlin.de>
2024-06-13 14:27:27 +0200

    moved heredoc_ctrl to separate function

--- src/parser/heredoc.c
+++ src/parser/heredoc.c
@@ -28,6 +28,30 @@ void read_heredoc_simple(char *delimiter, t_data *data)
 	close(fd);
 }
 
+int handle_ctrl_heredoc(char *line, t_cmd *current, t_data *data)
+{
+    if (!line || errno == EINVAL)
+    {
+        printf(MAG "you have pressed CTRL-D\n" RESET);
+        g_signal = 0; // Reset g_signal
+        data->exit_code = 130;
+        return 1;
+    }
+    if (g_signal == 2)
+    {
+        printf(MAG "\nCTRL+C detected. Exiting heredoc...\n" RESET);
+        if(ft_strcmp(current->args[0], "cat") == 0)
+            g_signal = 2; // Reset g_signal
+        else
+            g_signal = 0; // Reset g_signal
+        free(line);
+        data->exit_code = 130;
+        return 1; 
+	}
+	return 0;
+}
+
+
 void read_heredoc(char *delimiter, t_cmd *current, t_data *data)
 {
 	char *line;
@@ -38,24 +62,8 @@ void read_heredoc(char *delimiter, t_cmd *current, t_data *data)
 	while (1)
 	{
 		line = readline("> ");
-		if (!line || errno == EINVAL)
-		{
-			printf(MAG "you have pressed CTRL-D\n" RESET);
-			g_signal = 0; // need to reset g_signal, otherwise it will be 2 in the next iteration and the program will exit
-			data->exit_code = 130;
-			break;
-		}
-		if (g_signal == 2)
-		{
-        	printf(MAG"\nCTRL+C detected. Exiting heredoc...\n"RESET);
-			if(ft_strcmp(current->args[0], "cat") == 0)
-				g_signal = 2; // need to reset g_signal, otherwise it will be 2 in the next iteration and the program will exit
-			else
-				g_signal = 0; // need to reset g_signal, otherwise it will be 2 in the next iteration and the program will exit
-			free(line);
-			data->exit_code = 130;//// !important for exit code
+		if (handle_ctrl_heredoc(line,current, data))
             break;
-        }
 		if (ft_strcmp(line, delimiter) == 0)
 		{
 			data->exit_code = 0;
