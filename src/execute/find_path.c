f2af106bb315a9b6a4cd458b70063f8293b01e67
Daiana Rotchen <darotche@c4b11c1.42berlin.de>
2024-06-14 14:31:15 +0200

    relative and absolute paths and error messages

--- src/execute/find_path.c
+++ src/execute/find_path.c
@@ -1,31 +1,83 @@
 #include "minishell.h"
 
+char *check_abs_path(char *cmd, t_data *data)
+{
+    struct stat statbuf;
+
+    if (stat(cmd, &statbuf) == 0)
+    {
+        if (S_ISDIR(statbuf.st_mode))
+        {
+            errno = EISDIR; // Is a directory
+            fprintf(stderr, "-minishell: %s: Is a directory\n", cmd);
+            data->exit_code = 126;
+            return NULL;
+        }
+        else if (statbuf.st_mode & S_IXUSR)
+        {
+            return ft_strdup(cmd); // Command is executable
+        }
+        else
+        {
+            errno = EACCES; // Permission denied
+            fprintf(stderr, "-minishell: %s: Permission denied\n", cmd);
+            data->exit_code = 126;
+            return NULL;
+        }
+    }
+    return NULL;
+}
+
+// Function to check relative path using PATH environment variable
+char *check_rel_path(char *cmd, t_data *data)
+{
+    struct stat statbuf;
+    char *path;
+    char **paths;
+    int i;
+
+    paths = ft_split(ft_getenv("PATH", data->envs), ':');
+    i = 0;
+    while (paths[i])
+    {
+        path = ft_strjoin_nf(paths[i], "/");
+        path = ft_strjoin_nf(path, cmd);
+        if (stat(path, &statbuf) == 0)
+        {
+            free_arr2D(paths);
+            return path;
+        }
+        free(path);
+        i++;
+    }
+    free_arr2D(paths);
+    return NULL;
+}
+
+// Main function to find executable path
 char *find_path(char *cmd, t_data *data)
 {
 	char *path;
-	char **paths;
-	struct stat statbuf;
 
-	if (stat(cmd, &statbuf) == 0)
-		return (cmd);
-	if (ft_strcmp(cmd, "") == 0)
-		printf("-minishell: : command not found \n"); // This is a bit of a cheat code, if we have time we should make it better :)
-	paths = ft_split(ft_getenv("PATH", data->envs), ':');
-	int i = 0;
-	while (paths[i])
+    if (ft_strcmp(cmd, "") == 0)
+    {
+        errno = ENOENT; // No such file or directory
+        perror("-minishell");
+        data->exit_code = 127;
+        return NULL;
+    }
+	path = check_abs_path(cmd, data);
+    if (path != NULL)
+		return (path);
+	path = check_rel_path(cmd, data);
+	if (path != NULL)
+		return (path);
+	else
 	{
-		paths[i] = ft_strjoin_nf(paths[i], "/");
-		paths[i] = ft_strjoin_nf(paths[i], cmd);
-		if (stat(paths[i], &statbuf) == 0)
-		{
-			path = ft_strdup(paths[i]);
-			free_arr2D(paths);
-			return (path);
-		}
-		i++;
+		// If neither absolute nor relative path found
+		errno = ENOENT; // No such file or directory
+		fprintf(stderr, BLU "-minishell: %s: Command not found\n" RESET, cmd);
+		data->exit_code = 127;
 	}
-	free_arr2D(paths);
-	printf(BLU "-minishell: %s: command not found \n" RESET, cmd); // This error is for commands														   // add printferror for ("-minishell: %s: No such file or directory \n", cmd), there are some errors "Permision denied" w exit code 126(cmd found but not executable)
-	data->exit_code = 127;
-	return (NULL);
+    return NULL;
 }
