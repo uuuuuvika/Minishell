1b9b96470e0621e6bc67623505d4dff910e7c250
Daiana Rotchen <darotche@c4c1c1.42berlin.de>
2024-06-18 14:33:14 +0200

    error messages for non existing fd

--- src/builtins/ft_pwd.c
+++ src/builtins/ft_pwd.c
@@ -6,17 +6,17 @@
 /*   By: darotche <darotche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 13:40:53 by darotche          #+#    #+#             */
-/*   Updated: 2024/06/18 13:40:55 by darotche         ###   ########.fr       */
+/*   Updated: 2024/06/18 14:24:27 by darotche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void ft_pwd(t_data *data)
+void	ft_pwd(t_data *data)
 {
-	(void) data;
-	char cwd[MAXPATHLEN];
+	char	cwd[MAXPATHLEN];
 
+	(void) data;
 	if (getcwd(cwd, sizeof(cwd)) != NULL)
 	{
 		printf("%s\n", cwd);
--- src/builtins/ft_unset.c
+++ src/builtins/ft_unset.c
@@ -6,47 +6,47 @@
 /*   By: darotche <darotche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 13:41:01 by darotche          #+#    #+#             */
-/*   Updated: 2024/06/18 13:41:06 by darotche         ###   ########.fr       */
+/*   Updated: 2024/06/18 14:31:58 by darotche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void remove_env(t_data *data, char *arg)
+void	remove_env(t_data *data, char *arg)
 {
-    int i;
-	char *env_name;
+	int		i;
+	char	*env_name;
 
-    i = 0;
-    while (data->envs[i] != NULL)
-    {
+	i = 0;
+	while (data->envs[i] != NULL)
+	{
 		env_name = get_env_name(data->envs[i], '=');
  		//printf(GRN"env_name: %s\n"RESET, env_name);
-        if (ft_strcmp(env_name, arg) == 0)
-        {
-            free(data->envs[i++]);
-            while (data->envs[i] != NULL)
-            {
-                data->envs[i-1] = ft_strdup(data->envs[i]);
-                i++;
-            }
-            data->envs[i - 1] = NULL;
-			free(env_name);
-            return;
-        }
-        i++;
-    }
+		if (ft_strcmp(env_name, arg) == 0)
+		{
+			free(data->envs[i++]);
+			while (data->envs[i] != NULL)
+			{
+				data->envs[i - 1] = ft_strdup(data->envs[i]);
+				i++;
+			}
+			data->envs[i - 1] = NULL;
+			free (env_name);
+			return ;
+		}
+		i++;
+	}
 }
 
-void ft_unset(t_data *data, t_cmd *cmd)
+void	ft_unset(t_data *data, t_cmd *cmd)
 {
-    int i;
+	int	i;
 
-    i = 1;
-    while (cmd->args[i] != NULL)
-    {
-        remove_env(data, cmd->args[i]);
-        i++;
-    }
+	i = 1;
+	while (cmd->args[i] != NULL)
+	{
+		remove_env(data, cmd->args[i]);
+		i++;
+	}
 	data->exit_code = 0;
 }
