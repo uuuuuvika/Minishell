4a51f1ba9e3be36b68f9139009cdb878c3db0ef0
vika <vikashcherbaka@gmail.com>
2024-07-07 03:12:52 +0200

    fix cat <<

--- src/parser/redirections/redirections.c
+++ src/parser/redirections/redirections.c
@@ -6,21 +6,21 @@
 /*   By: vshcherb <vshcherb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/07 01:54:53 by vshcherb          #+#    #+#             */
-/*   Updated: 2024/07/07 01:59:07 by vshcherb         ###   ########.fr       */
+/*   Updated: 2024/07/07 03:12:07 by vshcherb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	process_redirections(t_cmd *current, int *i)
+int process_redirections(t_cmd *current, int *i)
 {
-	int	result;
+	int result;
 
 	if (ft_strcmp(current->args[*i], ">") == 0)
 	{
 		result = process_redirection(current, i, O_WRONLY
 				| O_CREAT | O_TRUNC, 1);
-		if (result != 0) 
+		if (result != 0)
 			return (result);
 	}
 	else if (ft_strcmp(current->args[*i], "<") == 0)
@@ -31,7 +31,7 @@ int	process_redirections(t_cmd *current, int *i)
 	}
 	else if (ft_strcmp(current->args[*i], ">>") == 0)
 	{
-		result = process_redirection(current, i, O_WRONLY 
+		result = process_redirection(current, i, O_WRONLY
 				| O_CREAT | O_APPEND, 1);
 		if (result != 0)
 			return (result);
@@ -39,24 +39,24 @@ int	process_redirections(t_cmd *current, int *i)
 	return (0);
 }
 
-int	redirect_assign(t_cmd *current, t_data *data)
+int redirect_assign(t_cmd *current, t_data *data)
 {
-	int	i;
-	int	result;
+	int i;
+	int result;
 
 	i = 0;
 	while (current->args[i])
 	{
-		result = process_redirections(current, &i);
-		if (result != -1)
+		if (is_redirect(current->args[i]))
 		{
+			if (ft_strcmp(current->args[i], "<<") == 0)
+			{
+				read_heredoc(current->args[i + 1], current, data);
+				i++;
+			}
+			result = process_redirections(current, &i);
 			if (result != 0)
 				return (result);
-		}
-		else if (ft_strcmp(current->args[i], "<<") == 0)
-		{
-			read_heredoc(current->args[i + 1], current, data);
-			i++;
 		}
 		i++;
 	}
